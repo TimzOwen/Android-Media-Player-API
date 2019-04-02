8d93e91c4a896e33aaa76c1b742913b33e673bf9
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-04-02 10:06:44 -0400

    Update MainActivity.java

--- app/src/main/java/com/owen/playmusic/MainActivity.java
+++ app/src/main/java/com/owen/playmusic/MainActivity.java
@@ -16,14 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//find the ids of all the buttons relate to the click action
         btnPlay = (Button) findViewById(R.id.btn_Play);
         btnPause = (Button) findViewById(R.id.btn_Pause);
         btnReset = (Button) findViewById(R.id.btn_Reset);
         btnStop = (Button)findViewById(R.id.btn_Stop);
-
+//immeadiately create / prepare the media file when the app is launch
         mediaPlayer = MediaPlayer.create(this, R.raw.verynicetumdo);
-
+//start the media player from the beginneing
         btnPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
                 mediaPlayer.start();
             }
         });
-
+//stops the ,usic . and resummes from the last stop
         btnPause.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v)
@@ -50,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
+                //resets the song to its origin start position
                 mediaPlayer.reset();
             }
         });
