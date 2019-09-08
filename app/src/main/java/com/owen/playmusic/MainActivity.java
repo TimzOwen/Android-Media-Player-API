78df395333bb49bfff05e48a95ece9c36ccd2d13
Timz Owen <ktimothy@kabarak.ac.ke>
2019-09-08 09:13:35 -0700

    Update MainActivity.java
    updated the http and local resource player

--- app/src/main/java/com/owen/playmusic/MainActivity.java
+++ app/src/main/java/com/owen/playmusic/MainActivity.java
@@ -71,3 +71,63 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 }
+
+package com.owen.mediaplayerapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnPlay;
+    Button btnPause;
+    MediaPlayer mediaPlayer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Create the Media player to play from Local raw file
+        mediaPlayer = MediaPlayer.create(this,R.raw.diamondeneka );
+
+        //create the music to play from the URI sourced system
+        Uri myUri = new Uri(recreate();
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(getApplicationContext(), myUri);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+
+        //platiny from requestion an HTTP . Make sure to have eventHandlers to avoid UI crushing
+        String player = "https:www.mdudo.com/diamond";
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(player);
+        mediaPlayer.prepare();
+        mediaPlayer.start();
+
+        btnPlay = findViewById(R.id.play_music);
+        btnPause = findViewById(R.id.pause_music);
+
+        //play the music as a raw local file
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.start();
+            }
+        });
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mediaPlayer.pause();
+            }
+        });
+    }
+}
